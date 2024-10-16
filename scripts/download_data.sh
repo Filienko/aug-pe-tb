ce34bf9da40fe29d870eec0a9516012450d23f8c
Daniil <94884910+Filienko@users.noreply.github.com>
2024-10-15 18:57:38 -0700

    Returning

--- scripts/download_data.sh
+++ scripts/download_data.sh
@@ -1,5 +1,5 @@
 pip install gdown
 # cd data/yelp
 # gdown https://drive.google.com/uc?id=1epLuBxCk5MGnm1GiIfLcTcr-tKgjCrc2
-cd ../tb
+cd data/tb
 gdown https://drive.google.com/uc?id=12-zV93MQNPvM_ORUoahZ2n4odkkOXD-r
